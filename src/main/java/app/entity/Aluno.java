d91b4eb7111ece0299580c1e463b7e48e3fe7345
chris <christianpimpo304@hotmail.com>
2025-06-18 06:35:55 -0300

    initial

---
+++ src/main/java/app/entity/Aluno.java
@@ -0,0 +1,17 @@
+package app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+
+}
